63a0b6892ea9cba40073370285770a9861df77ba
yusha1439 <yushalee10@gmail.com>
2018-05-24 00:25:22 -0500

    Create swap.sh

---
+++ scripts/swap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo && echo "Adding swap space..."
+        sleep 3
+        sudo fallocate -l 2G /swapfile
+        sudo chmod 600 /swapfile
+        sudo mkswap /swapfile
+        sudo swapon /swapfile
+        echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+        sudo sysctl vm.swappiness=10
+        sudo sysctl vm.vfs_cache_pressure=50
+        echo 'vm.swappiness=10' | sudo tee -a /etc/sysctl.conf
+        echo 'vm.vfs_cache_pressure=50' | sudo tee -a /etc/sysctl.conf
